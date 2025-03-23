c4085d6dbf61441b5c21cf8766d4620f52811f56
amyq96 <amyqin@gmail.com>
2025-03-23 12:12:06 -0500

    added tabs

---
+++ script.js
@@ -0,0 +1,14 @@
+function openTab(event, tabName) {
+    let tabContent = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    let tabLinks = document.getElementsByClassName("tab-link");
+    for (let i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.classList.add("active");
+}
